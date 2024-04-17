084482e1df51446d16e7774a7744f9199ce6480a
Eduardo Henrique <edu.hen.fm@gmail.com>
2024-04-16 22:38:45 -0400

    :feat: implementa a função para listar ativos, passando no teste

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+)
 
 type Ativo struct {
 	Codigo        string
 	Quantidade    int
 	ValorUnitario float64
+	DataDaCompra  time.Time
 }
 
 type Carteira []Ativo
@@ -26,6 +32,41 @@ func (c Carteira) AdicionarAtivo(a Ativo) error {
 	c = append(c, a)
 
 	return nil
+}
+
+func (c Carteira) ImprimirAtivos() string {
+	var sb strings.Builder
+	ativosPorCodigo := map[string][]Ativo{}
+
+	for _, a := range c {
+		ativosPorCodigo[a.Codigo] = append(ativosPorCodigo[a.Codigo], a)
+	}
+
+	sb.WriteString("Ativos\n")
+	sb.WriteString("-------------------------------------------\n")
+	sb.WriteString("|          Codigo |      Quantidade Total |\n")
+	sb.WriteString("-------------------------------------------\n")
+
+	for cod, ativos := range ativosPorCodigo {
+		sb.WriteString(fmt.Sprintf("| %15s | %21d |\n", cod, len(ativos)))
+		sb.WriteString("-------------------------------------------\n")
+	}
+
+	sb.WriteString("\n")
+
+	for cod, ativos := range ativosPorCodigo {
+		sb.WriteString(fmt.Sprintf("%s\n", cod))
+		sb.WriteString("-------------------------------------------\n")
+		sb.WriteString("|  Valor Unitario |        Data da Compra |\n")
+		sb.WriteString("-------------------------------------------\n")
+
+		for _, a := range ativos {
+			sb.WriteString(fmt.Sprintf("| %15.2f | %21s |\n", a.ValorUnitario, a.DataDaCompra.Format("2006-01-02")))
+			sb.WriteString("-------------------------------------------\n")
+		}
+	}
+
+	return sb.String()
 }
 
 func main() {}
